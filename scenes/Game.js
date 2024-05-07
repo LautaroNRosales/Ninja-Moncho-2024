0226a01123dbc8567d2b7bfc2aeb318de398ad27
LautaroNRosales <lautarounraf@gmail.com>
2024-05-07 18:37:58 -0300

    Cambios de Nombres

---
+++ scenes/Game.js
@@ -0,0 +1,15 @@
+// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("mainh");
+  }
+
+  init() {}
+
+  preload() {}
+
+  create() {}
+
+  update() {}
+}
